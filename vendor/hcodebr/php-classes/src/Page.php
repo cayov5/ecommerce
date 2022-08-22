0d6bd9f193f7d307b9a2ecb8aee2a19270f6a0b2
Cayo Vinicyus <cayov5@gmail.com>
2022-08-22 17:26:29 -0300

    criamos pageadmin e o tpl admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -5,12 +5,12 @@ class Page{
     private $tpl;
     private $options;
     private $deafults =["data"=>[] ];
-    public function __construct($opts = array()){
+    public function __construct($opts = array(), $tpl_dir = "/views/"){
 
         $this->options = array_merge($this->deafults, $opts);
 
         $config = array(
-            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/", 
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir, 
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
             "debug"         => false 
             );
